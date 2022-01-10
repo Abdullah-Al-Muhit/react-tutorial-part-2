ac7ff54c4b530436271c02474a968f3d0ee97261
MuhitMridul <muhitmridul71@gmail.com>
2022-01-10 18:29:35 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// LECTURE 5
+// const index = 0;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// setInterval(() => {
+//   const element = (
+//     <h1 className='heading' tabIndex={index}>
+//       <span className='text'>hello {new Date().toLocaleTimeString()}</span>
+//       <img src="" alt="" />
+//     </h1>
+//   )
+//   ReactDOM.render(element, document.getElementById('root'));
+
+// }, 1000);
+
+// LECTURE 6
+
+class Clock extends React.Component {
+  render() {
+    return (
+      <h1 className='heading'>
+        <span className="text">
+          hello - {this.props.children} {new Date().toLocaleTimeString(this.props.locale)}
+        </span>
+      </h1>
+    )
+  }
+}
+
+ReactDOM.render(<clock locale='bn-BD'>test<clock/>, document.getElementById('root'));
